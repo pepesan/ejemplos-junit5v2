e04d18365d4297f377b883c18c94c73489bfedd3
David Vaquero <pepesan@gmail.com>
2021-09-24 12:16:11 +0200

    corregido error de mockito

--- src/test/java/ejercicios/mockito/ModeloTest.java
+++ src/test/java/ejercicios/mockito/ModeloTest.java
@@ -3,6 +3,7 @@ package ejercicios.mockito;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class ModeloTest {
