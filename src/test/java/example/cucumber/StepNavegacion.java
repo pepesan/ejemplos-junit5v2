3200c8966b79ebbdb9b46a19db70bf101a9b85ca
David Vaquero <pepesan@gmail.com>
2021-09-24 12:24:26 +0200

    metido otro ejemplo de madrid decide

--- src/test/java/example/cucumber/StepNavegacion.java
+++ src/test/java/example/cucumber/StepNavegacion.java
@@ -12,6 +12,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.JavascriptExecutor;
 import java.util.*;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class StepNavegacion {
     private WebDriver driver;
     private Map<String, Object> vars;
@@ -47,5 +50,24 @@ public class StepNavegacion {
     @And("Cierro el navegador")
     public void cierroElNavegador() {
         driver.quit();
+    }
+
+
+    @When("Voy a la pagina de usera")
+    public void voyALaPaginaDeUsera() {
+        driver.get("https://decide.madrid.es/presupuestos");
+        driver.findElement(By.cssSelector(".shadow_squared:nth-child(19) a")).click();
+    }
+
+    @When("hago click en la primera propuesta")
+    public void hagoClickEnLaPrimeraPropuesta() {
+        driver.findElement(By.linkText("Paso peatonal a la entrada trasera del colegio Edith Stein")).click();
+        driver.findElement(By.cssSelector("h1:nth-child(1)")).click();
+    }
+
+    @Then("Compruebo los titulos")
+    public void comprueboLosTitulos() {
+        assertThat(driver.getTitle(), is("Paso peatonal a la entrada trasera del colegio Edith Stein"));
+        assertThat(driver.findElement(By.cssSelector("#investment_code > strong")).getText(), is("17045"));
     }
 }
---
+++ src/test/resources/example/cucumber/madriddecide.feature
@@ -0,0 +1,9 @@
+Feature: Prueba de Madrid decide
+  Prueba de uso de Webdriver para navegare sobre Madrid Decide
+  Background:
+    Given Abro el navegador en la pagina web principal
+  Scenario: La navegación a un distrito es correcta
+    When Voy a la pagina de usera
+    When hago click en la primera propuesta
+    Then Compruebo los titulos
+    And Cierro el navegador
