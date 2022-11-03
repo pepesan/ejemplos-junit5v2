f1c3ccdd37cea99fbaff0c2578ffdb0cbf6c8500
David Vaquero <pepesan@gmail.com>
2022-11-03 13:25:44 +0100

    mejoras menores

--- src/test/java/example/SimpleTest.java
+++ src/test/java/example/SimpleTest.java
@@ -2,7 +2,7 @@ package example;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SimpleTest {
     @Test
