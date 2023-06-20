9202b65897ba64b20323ce2f4ddf73f412ede960
David Vaquero <pepesan@gmail.com>
2023-06-20 12:55:08 +0200

    mejoras de los ejemplos de suite con tags

--- src/test/java/example/tags/IntegracionTest.java
+++ src/test/java/example/tags/IntegracionTest.java
@@ -1,13 +1,12 @@
 package example.tags;
 
 import org.junit.platform.runner.JUnitPlatform;
-import org.junit.platform.suite.api.*;
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
 
-@Suite
-@SuiteDisplayName("JUnit Platform Suite Demo")
-@SelectPackages("example")
-// @IncludeClassNamePatterns(".*Test")
-@IncludeTags("integracion")
+@RunWith(JUnitPlatform.class)
+@SelectPackages("example.tags")  // Paquete que contiene las clases de prueba
+@IncludeTags({"unidad", "smoke"})  // Tags que se deben ejecutar
 public class IntegracionTest {
 }
