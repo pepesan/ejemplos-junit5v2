35f3f2a3b23750582c9e259215dd0f5754d871d7
David Vaquero <pepesan@gmail.com>
2023-06-20 14:21:30 +0200

    mejoras de los ejemplo de mockito actualizadas las dependencias por un fallo de incompatibilidad entre versiones de mockito y byte-buddy

--- src/test/java/example/integracion/CajaObjetoIntegrationTest.java
+++ src/test/java/example/integracion/CajaObjetoIntegrationTest.java
@@ -6,19 +6,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class CajaObjetoIntegrationTest {
+
     @Test
     public void testIntegracionCajaObjeto(){
         // declaro el dato que quiero devolver
         String datoADevolver = "Micadena";
-        // Declarar un objeto de la clase Caja
-        Caja c = new Caja();
         // creo el objeto simulado
         Objeto objetoSimulado = spy(new Objeto());
-        // cambio el objeto original por el simulado
-        c.setObjeto(objetoSimulado);
+
         // cambio el comportamiento del objeto simulado para que
         // haga lo que yo le digo y no lo de por defecto
         when(objetoSimulado.getCadena()).thenReturn(datoADevolver);
+
+        // Opción 1: Inicializar con objjeto simulado
+        // declaro el objeto de caja inicializando con la constructora con parámetros
+        Caja c = new Caja(objetoSimulado);
+
+        // Opción 2: utilizar un setter para pasar el objeto simulado
+        // Declarar un objeto de la clase Caja
+        c = new Caja();
+        // cambio el objeto original por el simulado
+        c.setObjeto(objetoSimulado);
+
         // llamamos al método que debería hacer la llamada al objeto simulado
         c.procesaDatos();
         // comprobar que se procesado la llamada al objeto simulado
--- src/test/java/example/integracion/CajaTest.java
+++ src/test/java/example/integracion/CajaTest.java
@@ -10,19 +10,28 @@ public class CajaTest {
     public void testConstructor(){
         Caja caja = new Caja();
         assertNull(caja.getObjeto());
+        assertNull(caja.getDatosProcesados());
     }
     @Test
     public void testConstructorConParametro(){
         Objeto objeto = new Objeto();
         Caja caja = new Caja(objeto);
         assertEquals(objeto, caja.getObjeto());
+        assertNull(caja.getDatosProcesados());
     }
     @Test
-    public void testSetter(){
+    public void testSetterObjeto(){
         Objeto objeto = new Objeto();
         Caja caja = new Caja();
         caja.setObjeto(objeto);
         assertEquals(objeto, caja.getObjeto());
+    }
+    @Test
+    public void testSetterDatosProcesados(){
+        String dato = "Dato";
+        Caja caja = new Caja();
+        caja.setDatosProcesados(dato);
+        assertEquals(dato, caja.getDatosProcesados());
     }
 
 }
